eb1c7eafcb102ecd2a63b4612db1e6e160f01799
Alex Southgate <southgate@gmail.com>
2011-04-12 00:05:22 -0700

    updated search syntax

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-HotHubs!
+HotHubs
+
+
+User
+  - username
+  Snapshot
+  - number of followers
+  - timestamp
+
+Repo
+  - username
+  - name
+  Snapshot
+  - followers
--- crawl.rb
+++ crawl.rb
@@ -2,6 +2,20 @@ require 'httparty'
 require 'pp'
 require 'json'
 
+require 'uri'
+
+# Fredom patch URI so it doesn't puke on [ or ] coming from HTTParty
+module URI
+  class << self
+
+    def parse_with_safety(uri)
+      URI::Parser.new(:UNRESERVED=>URI::PATTERN::UNRESERVED+'\[\]').parse uri
+    end
+    alias parse_without_safety parse
+    alias parse parse_with_safety
+  end
+end
+
 class GitHub
   include HTTParty
 
@@ -9,14 +23,20 @@ class GitHub
 
   REPOS_URI = base_uri + "/repos/search"
 
-  def self.repos(term, page = 1)
-    uri = GitHub::REPOS_URI + "/#{term}?start_page=#{page}"
-    response = GitHub.get(uri)
-    parsed = JSON.parse(response.body)
-    parsed['repositories'] if parsed.has_key? 'repositories'
+  class << self
+    def parser
+      URI::Parser.new(:UNRESERVED=>URI::PATTERN::UNRESERVED+'\[\]')
+    end
+
+    def repos(term, page = 1)
+      uri = GitHub::REPOS_URI + "/#{URI.encode(term)}?start_page=#{page}"
+      response = GitHub.get(uri)
+      parsed = JSON.parse(response.body)
+      parsed['repositories'] if parsed.has_key? 'repositories'
+    end
   end
 end
 
 
-repos = GitHub.repos("*")
+repos = GitHub.repos("followers:[5 TO 100]")
 pp repos.map { |repo| repo['name'] }
